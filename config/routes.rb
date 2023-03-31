a85279bab2399862cb1fdb9877145c1f8a497783
harshphogat <harshphogat2001@gmail.com>
2023-03-31 12:56:26 +0530

    Active admin gem integrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :articles
   root 'pages#test'
   get 'signup', to: 'users#new'
